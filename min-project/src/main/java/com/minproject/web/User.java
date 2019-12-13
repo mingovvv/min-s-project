668548a7fe6d3adea3548ba8544311d6bd9e10ee
puregyu <puregyu@naver.com>
2019-12-13 16:48:33 +0900

    dev_min[회원가입 & 사용자 목록 구현]

---
+++ min-project/src/main/java/com/minproject/web/User.java
@@ -0,0 +1,42 @@
+package com.minproject.web;
+
+public class User {
+	private String userId;
+	private String userName;
+	private String userPassword;
+	
+	public User() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public String getUserId() {
+		return userId;
+	}
+
+	public void setUserId(String userId) {
+		this.userId = userId;
+	}
+
+	public String getUserName() {
+		return userName;
+	}
+
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+
+	public String getUserPassword() {
+		return userPassword;
+	}
+
+	public void setUserPassword(String userPassword) {
+		this.userPassword = userPassword;
+	}
+
+	@Override
+	public String toString() {
+		return "User [userId=" + userId + ", userName=" + userName + ", userPassword=" + userPassword + "]";
+	}
+	
+	
+}
